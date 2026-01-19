72835d52057b0ec964febeeedba03287f26df92d
Rory Duncan <robust.rory@gmail.com>
2026-01-18 17:26:06 -0800

    Add lint and prettier

--- packages/transpiler/eslint.config.mjs
+++ packages/transpiler/eslint.config.mjs
@@ -1,10 +1,12 @@
 import eslint from '@eslint/js';
+import prettier from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
+  prettier,
   {
     languageOptions: {
       parserOptions: {
@@ -27,7 +29,7 @@ export default tseslint.config(
     },
   },
   {
-    ignores: ['dist/**', 'node_modules/**', 'coverage/**', '*.config.*'],
+    ignores: ['dist/**', 'node_modules/**', 'coverage/**', 'tests/**', '*.config.*'],
   }
 );
 
